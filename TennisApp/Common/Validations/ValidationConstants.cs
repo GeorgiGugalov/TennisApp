ebc0ca25f7c843baefd5e1cc019ade68b7203392
Gosho <Gosho@DESKTOP-3IMUUA7>
2024-11-24 19:39:35 +0200

    Create ValidationConstants class and GlobalConstants class.

---
+++ TennisApp/Common/Validations/ValidationConstants.cs
@@ -0,0 +1,17 @@
+﻿namespace TennisApp.Common.Validations
+{
+    public class ValidationConstants
+    {
+        //Common
+        public const string BirthDateFormat = "dd-MM-YYYY";
+        public const int FullNameMinLength = 3;
+        public const int FullNameMaxLength = 50;
+
+        //Booking
+        public const string BookingDateFormat = "dd/MM HH:mm";
+
+        //Court
+        public const int CourtNameMinLength = 1;
+        public const int CourtNameMaxLength = 50;
+    }
+}

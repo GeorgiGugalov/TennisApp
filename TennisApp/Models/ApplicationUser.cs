b3232cf288c202a152ed719308f0d91958b4f36f
Gosho <Gosho@DESKTOP-3IMUUA7>
2024-11-24 19:41:06 +0200

    Creating basic models and their properties validations + adding Enums.

---
+++ TennisApp/Models/ApplicationUser.cs
@@ -0,0 +1,40 @@
+﻿using TennisApp.Data.Common;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TennisApp.Models
+{
+    public class ApplicationUser : IdentityUser, IDeletableEntity
+    {
+        public ApplicationUser()
+        {
+            this.Id = Guid.NewGuid().ToString();
+            this.Roles = new HashSet<IdentityUserRole<string>>();
+            this.Claims = new HashSet<IdentityUserClaim<string>>();
+            this.Logins = new HashSet<IdentityUserLogin<string>>();
+        }
+        // Common properties for both coaches and members
+        [Required]
+        public string FullName { get; set; } = null!;
+
+        public string ProfileImageUrl { get; set; } = string.Empty;
+
+        // Foreign key for coach
+        public string? CoachId { get; set; }
+        public ApplicationUser? Coach { get; set; }
+
+        public bool IsDeleted { get; set; } = false;
+        public DateTime? DeletedOn { get; set; }
+
+        public virtual ICollection<IdentityUserRole<string>> Roles { get; set; }
+
+        public virtual ICollection<IdentityUserClaim<string>> Claims { get; set; }
+
+        public virtual ICollection<IdentityUserLogin<string>> Logins { get; set; }
+    }
+}
---
+++ TennisApp/Models/Setting.cs
@@ -0,0 +1,11 @@
+﻿namespace TennisApp.Models
+{
+    using TennisApp.Data.Common;
+
+    public class Setting : BaseDeletableModel<int>
+    {
+        public string Name { get; set; }
+
+        public string Value { get; set; }
+    }
+}

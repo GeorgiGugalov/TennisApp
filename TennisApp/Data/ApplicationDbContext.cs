88e0946729d082a4502bf4ee6038537ca78f3a91
Gosho <Gosho@DESKTOP-3IMUUA7>
2024-11-24 19:43:29 +0200

    Adding DbSets for entities.

--- TennisApp/Data/ApplicationDbContext.cs
+++ TennisApp/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using TennisApp.Models;
 
 namespace TennisApp.Data
 {
@@ -9,5 +10,13 @@ namespace TennisApp.Data
             : base(options)
         {
         }
+
+        public DbSet<Booking> Bookings { get; set; }
+        public DbSet<Coach> Coaches { get; set; }
+        public DbSet<CoachLesson> CoachLessons { get; set; }
+        public DbSet<Court> Courts { get; set; }
+        public DbSet<Lesson> Lessons { get; set; }
+        public DbSet<Member> Members { get; set; }
+        public DbSet<Setting> Settings { get; set; }
     }
 }

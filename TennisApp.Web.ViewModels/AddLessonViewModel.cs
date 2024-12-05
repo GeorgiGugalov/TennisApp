4a91a74755c7ecc2f466bbbd5337d2ff07f07792
Gosho <Gosho@DESKTOP-3IMUUA7>
2024-12-05 19:51:13 +0200

    New architecture.

---
+++ TennisApp.Web.ViewModels/AddLessonViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using TennisApp.Data.Models;
+
+namespace TennisApp.Models
+{
+    public class AddLessonViewModel
+    {
+        [Required]
+        public Guid MemberId { get; set; }
+        public Member? Member { get; set; }
+        [Required]
+        public Guid CourtId { get; set; }
+        public Court? Court { get; set; }
+        [Required]
+        public Guid CoachId { get; set; }
+        public Coach? Coach { get; set; }
+
+    }
+}
